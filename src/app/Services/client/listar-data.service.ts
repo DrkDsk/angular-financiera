3335b222fb5a8df9cd85bf267813efb10ccea53c
drkdsk <joseaph.1998@gmail.com>
2021-03-03 20:36:45 -0600

    se elimina funcion innecesaria

--- src/app/Services/client/listar-data.service.ts
+++ src/app/Services/client/listar-data.service.ts
@@ -10,18 +10,7 @@ export class ListarDataService {
 
   private urlApi = "http://127.0.0.1:8000/api/"
 
-  headerName = 'X-XSRF-TOKEN';
-
   constructor(private http:HttpClient) { }
-
-  getAllClients():Observable<Client[]>{
-    const ops = {
-      headers : new HttpHeaders({
-        'X-Requested-With' : 'HttpClient' 
-      })
-    };
-    return this.http.get<Client[]>(this.urlApi+"clients",ops);
-  }
   
   getClients():Observable<Client[]>{
     return this.http.get<Client[]>(this.urlApi+"clients");
@@ -29,5 +18,9 @@ export class ListarDataService {
 
   deleteClient(client:Client){
     return this.http.delete<Client[]>(this.urlApi+"clients/"+client.id)
+  }
+
+  addClient(client:Client){
+    return this.http.post<Client>(this.urlApi + 'clients',client)
   }
 }
