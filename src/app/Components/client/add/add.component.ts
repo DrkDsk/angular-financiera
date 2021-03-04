0fa74fc00522831260f56090364b509ac8950895
drkdsk <joseaph.1998@gmail.com>
2021-03-03 20:36:08 -0600

    componente add

---
+++ src/app/Components/client/add/add.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router'
+import { ListarDataService } from 'src/app/Services/client/listar-data.service';
+import { Client } from '../../../Models/Client/client.interface'
+
+@Component({
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+
+export class AddComponent implements OnInit {
+
+  clients:Client[] = [];
+  client = new Client();
+
+  ngOnInit(): void {
+  }
+  
+  constructor(private router: Router, private service:ListarDataService) { }
+
+  getClients(){
+    this.service.getClients().subscribe( data => {
+      this.clients = data;
+    })
+  }
+
+  saveClient(){
+    this.service.addClient(this.client).subscribe( data => {
+      this.router.navigate([''])
+    })
+  }
+}
