2e9f12cd92e4eada8c608afb34827f8501e3b636
drkdsk <joseaph.1998@gmail.com>
2021-03-03 20:37:11 -0600

    se añaden ruta raiz y ruta add

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { from } from 'rxjs';
+import { AddComponent } from './Components/client/add/add.component';
+import { DashboardComponent} from './Components/dashboard/dashboard.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'add', component: AddComponent},
+  {path: '' , component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
