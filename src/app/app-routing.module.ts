e64a8c9fedc8670e873ae8ea3eab5103bca3f511
drkdsk <joseaph.1998@gmail.com>
2021-03-05 15:46:16 -0600

    añadiendo componente editar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { from } from 'rxjs';
 import { AddComponent } from './Components/client/add/add.component';
+import { EditComponent } from './Components/client/edit/edit.component';
 import { DashboardComponent} from './Components/dashboard/dashboard.component'
 
 const routes: Routes = [
   {path: 'add', component: AddComponent},
-  {path: '' , component: DashboardComponent}
+  {path: 'edit', component: EditComponent},
+  {path: '' , component: DashboardComponent},
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
