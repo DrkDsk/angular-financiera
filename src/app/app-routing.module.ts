2aba13aca85719644cf3bb72145869f1b485959c
drkdsk <joseaph.1998@gmail.com>
2021-03-27 15:41:05 -0600

    se agregan rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './Components/client/add/add.component';
 import { EditComponent } from './Components/client/edit/edit.component';
 import { DashboardComponent} from './Components/dashboard/dashboard.component'
+import { AddLoanComponent} from './Components/loan/add-loan/add-loan.component'
+import { GetLoansComponent} from './Components/loan/get-loans/get-loans.component'
 
 const routes: Routes = [
-  {path: 'add', component: AddComponent},
-  {path: 'edit', component: EditComponent},
-  {path: '' , component: DashboardComponent},
+  {path: 'clients' , component: DashboardComponent},
+  {path: 'clients/add', component: AddComponent},
+  {path: 'clients/edit', component: EditComponent},
+  {path: 'loans', component: GetLoansComponent},
+  {path: 'loans/add', component: AddLoanComponent}
 ];
 
 @NgModule({
