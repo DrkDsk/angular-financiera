2163fcd77d69f3c0ebfb94d8e963196ccc7fe292
drkdsk <joseaph.1998@gmail.com>
2021-03-27 15:44:32 -0600

    interfaz de loans

---
+++ src/app/Models/Loan/loan.interface.ts
@@ -0,0 +1,9 @@
+export class Loan{
+    id: string = ""
+    name: string = ""
+    amount: number = 0
+    payments_number: number = 0
+    fee : number = 0
+    ministry_date: Date = new Date
+    due_date : Date = new Date
+}
