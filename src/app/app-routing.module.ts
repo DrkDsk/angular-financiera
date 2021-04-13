54a7ed15e0612310c76ac1d6f5be68ccd77a2f8d
drkdsk <joseaph.1998@gmail.com>
2021-04-13 13:40:25 -0500

    ruta raíz agregada

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,15 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 import { AddComponent } from './Components/client/add/add.component';
 import { EditComponent } from './Components/client/edit/edit.component';
 import { DashboardComponent} from './Components/dashboard/dashboard.component'
+import { HomeComponent } from './Components/home/home.component';
 import { AddLoanComponent} from './Components/loan/add-loan/add-loan.component'
 import { GetLoansComponent} from './Components/loan/get-loans/get-loans.component'
+import { PaymentComponent } from './Components/payment/payment.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'clients' , component: DashboardComponent},
   {path: 'clients/add', component: AddComponent},
   {path: 'clients/edit', component: EditComponent},
   {path: 'loans', component: GetLoansComponent},
-  {path: 'loans/add', component: AddLoanComponent}
+  {path: 'loans/add', component: AddLoanComponent},
+  {path: 'payments', component: PaymentComponent}
 ];
 
 @NgModule({
