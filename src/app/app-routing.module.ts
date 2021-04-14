841539c708b3d40db4a583d4faaa1f64936d687b
drkdsk <joseaph.1998@gmail.com>
2021-04-14 11:41:26 -0500

    ruta payments añadida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { DashboardComponent} from './Components/dashboard/dashboard.component'
 import { HomeComponent } from './Components/home/home.component';
 import { AddLoanComponent} from './Components/loan/add-loan/add-loan.component'
 import { GetLoansComponent} from './Components/loan/get-loans/get-loans.component'
-import { PaymentComponent } from './Components/payment/payment.component';
+import { GetPaymentsComponent } from './Components/payment/get-payments/get-payments.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
@@ -15,7 +15,7 @@ const routes: Routes = [
   {path: 'clients/edit', component: EditComponent},
   {path: 'loans', component: GetLoansComponent},
   {path: 'loans/add', component: AddLoanComponent},
-  {path: 'payments', component: PaymentComponent}
+  {path: 'payments', component: GetPaymentsComponent}
 ];
 
 @NgModule({
