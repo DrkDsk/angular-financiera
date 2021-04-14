83a9440ed4c2f1980d51120e9a12aa5a77558c29
drkdsk <joseaph.1998@gmail.com>
2021-04-14 13:39:05 -0500

    actualizando rutas y componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AddComponent } from './Components/client/add/add.component';
-import { EditComponent } from './Components/client/edit/edit.component';
-import { DashboardComponent} from './Components/dashboard/dashboard.component'
+import { AddClientsComponent } from './Components/client/add-clients/add-clients.component';
+import { EditClientsComponent } from './Components/client/edit-clients/edit-clients.component';
+import { GetClientsComponent } from './Components/client/get-clients/get-clients.component';
 import { HomeComponent } from './Components/home/home.component';
 import { AddLoanComponent} from './Components/loan/add-loan/add-loan.component'
 import { GetLoansComponent} from './Components/loan/get-loans/get-loans.component'
@@ -10,9 +10,9 @@ import { GetPaymentsComponent } from './Components/payment/get-payments/get-paym
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
-  {path: 'clients' , component: DashboardComponent},
-  {path: 'clients/add', component: AddComponent},
-  {path: 'clients/edit', component: EditComponent},
+  {path: 'clients' , component: GetClientsComponent},
+  {path: 'clients/add', component: AddClientsComponent},
+  {path: 'clients/edit', component: EditClientsComponent},
   {path: 'loans', component: GetLoansComponent},
   {path: 'loans/add', component: AddLoanComponent},
   {path: 'payments', component: GetPaymentsComponent}
